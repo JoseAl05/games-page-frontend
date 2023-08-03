f470397f26a537142ddd6ff4496c40f0ebad4cf7
JoseAl05 <jparancibialinker@gmail.com>
2023-08-02 22:55:54 -0400

    added router link and router link active

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
+  currentRoute: boolean = false;
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit(){
+    console.log(this.route.url);
+  }
 }
