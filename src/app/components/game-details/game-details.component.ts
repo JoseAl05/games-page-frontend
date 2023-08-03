5beec44c2ef0a59312f2ea7a2b791fe2178bab5b
JoseAl05 <jparancibialinker@gmail.com>
2023-08-02 22:55:38 -0400

    added ratings and metacritic score

--- src/app/components/game-details/game-details.component.ts
+++ src/app/components/game-details/game-details.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GameDetailsProps } from 'src/app/interfaces/game-details.interface';
 import { GameDetailsService } from 'src/app/services/game-details.service';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faRedditAlien } from '@fortawesome/free-brands-svg-icons';
+import { faComment } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-game-details',
@@ -13,11 +17,19 @@ export class GameDetailsComponent {
   game_slug: string;
   game_details: GameDetailsProps;
   isLoading: boolean = true;
+  metaScore: number;
+
+  faComment = faComment;
 
   constructor(
     private route: ActivatedRoute,
-    private gameDetailsService: GameDetailsService
-  ) {}
+    private gameDetailsService: GameDetailsService,
+    private library: FaIconLibrary,
+  ) {
+    //ERROR: Type '"threads"' is not assignable to type 'IconName'. This is why I added @ts-ignore
+    //@ts-ignore
+    library.addIcons(faRedditAlien);
+  }
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
@@ -33,6 +45,7 @@ export class GameDetailsComponent {
       .subscribe({
         next: (game_details) => {
           this.game_details = game_details;
+          this.metaScore = parseInt(game_details.metacritic);
           this.isLoading = false;
         },
         error: (e) => console.error(e),
