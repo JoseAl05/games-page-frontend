ccc1daf65295235d09e86c5dd8aef0c31dee364e
JoseAl05 <jparancibialinker@gmail.com>
2023-08-06 01:55:12 -0400

    created function to fetch screenshots

--- src/app/components/game-details/game-details.component.ts
+++ src/app/components/game-details/game-details.component.ts
@@ -5,7 +5,8 @@ import { GameDetailsService } from 'src/app/services/game-details.service';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faRedditAlien } from '@fortawesome/free-brands-svg-icons';
 import { faComment } from '@fortawesome/free-solid-svg-icons';
-
+import { ScreenshotsService } from 'src/app/services/screenshots.service';
+import { ScreenshotProps } from 'src/app/interfaces/screenshot.interface';
 
 @Component({
   selector: 'app-game-details',
@@ -16,13 +17,15 @@ export class GameDetailsComponent {
   game_id: string;
   game_slug: string;
   game_details: GameDetailsProps;
+  screenshots: ScreenshotProps;
   isLoading: boolean = true;
+  isLoadingImages: boolean = true;
   metaScore: number;
-
 
   constructor(
     private route: ActivatedRoute,
     private gameDetailsService: GameDetailsService,
+    private screenshotsService: ScreenshotsService
   ) {}
 
   ngOnInit() {
@@ -31,6 +34,7 @@ export class GameDetailsComponent {
       this.game_slug = params['game_slug'];
     });
     this.getGameDetails();
+    this.getScreenshots();
   }
 
   getGameDetails() {
@@ -41,6 +45,22 @@ export class GameDetailsComponent {
           this.game_details = game_details;
           this.metaScore = parseInt(game_details.metacritic);
           this.isLoading = false;
+        },
+        error: (e) => console.error(e),
+        complete: () => {
+          console.log('Completed');
+          this.isLoading = false;
+        },
+      });
+  }
+
+  getScreenshots() {
+    const screenshots = this.screenshotsService
+      .getScreenshots(this.game_id)
+      .subscribe({
+        next: (screenshots) => {
+          this.screenshots = screenshots;
+          this.isLoadingImages = false;
         },
         error: (e) => console.error(e),
         complete: () => {
