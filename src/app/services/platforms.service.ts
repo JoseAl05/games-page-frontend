e0f7e629024e13c348c80ed388decf2ac9423a95
JoseAl05 <jparancibialinker@gmail.com>
2023-08-06 01:56:22 -0400

    deleted unused import

--- src/app/services/platforms.service.ts
+++ src/app/services/platforms.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { GameProps } from '../interfaces/game.interface';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
